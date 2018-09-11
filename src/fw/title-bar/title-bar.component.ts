04a6502d5d0e80bff003957ffc04ad7890da09e4
Gary Cooper <garycooper6@hotmail.com>
2018-09-11 10:17:50 +0100

    Create Framework Config

--- src/fw/title-bar/title-bar.component.ts
+++ src/fw/title-bar/title-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'fw-title-bar',
   templateUrl: './title-bar.component.html',
   styleUrls: ['./title-bar.component.css']
@@ -11,5 +12,4 @@ export class TitleBarComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
